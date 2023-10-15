d9080e670556e9844114270d0f9593b6405fa207
dinozood <multiplydino@gmail.com>
2023-10-15 22:40:45 +0300

    second homework

--- ip_filter.h
+++ ip_filter.h
@@ -10,4 +10,16 @@
 
 std::vector<std::string> split(const std::string &str, char d);
 
+typedef struct ip_addr {
+    ip_addr();
+
+    explicit ip_addr(std::vector<std::string> addr_line);
+    void print() const;
+    uint8_t _byte[4];
+} ip_addr_ts;
+
+int ip_comp(const void* L_v, const void* R_v);
+
+
+
 #endif //HOMEWORK_2_IP_FILTER_H
--- test_cases.cpp
+++ test_cases.cpp
@@ -1,7 +1,46 @@
 #include "ip_filter.h"
 #include <gtest/gtest.h>
 
-TEST(ProjectVersionTest, SuitableVersion) {
-//    std::cout << version() << std::endl;
-//    EXPECT_GT(version(), 0);
+#define UPPER_LIMIT (256)
+
+void test_all_good() {
+    /*
+     * testing 2^64 variants is too much as for me
+     * */
+
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 10000; ++i) {
+        std::vector<std::string> tmp = {
+                std::to_string(std::rand() % UPPER_LIMIT),
+                std::to_string(std::rand() % UPPER_LIMIT),
+                std::to_string(std::rand() % UPPER_LIMIT),
+                std::to_string(std::rand() % UPPER_LIMIT),
+        };
+        ip_addr exmpl(tmp);
+    }
+}
+
+
+TEST(IpValidation, dontCatchExpressionsInFor) {
+    EXPECT_NO_THROW(test_all_good());
+}
+
+TEST(IpValidation, catchIt) {
+    for (int i = 0; i < 1000; ++i) {
+        std::srand(std::time(nullptr));
+        std::vector<std::string> ipAddr = {
+                std::to_string(std::rand() % UPPER_LIMIT),
+                std::to_string(std::rand() % UPPER_LIMIT),
+                std::to_string(std::rand() % UPPER_LIMIT),
+                std::to_string(std::rand() % UPPER_LIMIT),
+        };
+        ipAddr[std::rand() % 4] = std::to_string((std::rand() % UPPER_LIMIT) + UPPER_LIMIT);
+        EXPECT_ANY_THROW([[maybe_unused]] ip_addr tmp1 = ip_addr(ipAddr));
+    }
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
